44c91de56f5d8dede0212e9207eabd47baf52cf1
Tom Eichlersmith <tom.eichlersmith@gmail.com>
2018-01-04 13:08:25 -0600

    RandDouble compiles

--- src/RandDouble.cpp
+++ src/RandDouble.cpp
@@ -6,8 +6,12 @@ namespace MOSEY {
 	RandDouble::RandDouble(const double minimum, const double maximum) :
 				r( std::bind(
 					std::uniform_real_distribution<>( minimum , maximum ),
-					std::default_random_engine() ) ) { /* Intentionally Empty */ }
+					std::default_random_engine() ) ) {
+		/* Intentionally Empty */
+	}
 	
-	double RandDouble::operator()() { return r(); }
+	double RandDouble::operator()() {
+		return r();
+	}
 	
 }
