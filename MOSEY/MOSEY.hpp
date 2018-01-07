6bbba4dcdbd1668222d411194709839ff75e541a
Tom Eichlersmith <tom.eichlersmith@gmail.com>
2018-01-07 12:39:58 -0600

    addition of include file for entire package

---
+++ MOSEY/MOSEY.hpp
@@ -0,0 +1,16 @@
+/**
+ * Include file for package
+ */
+
+
+#include "CoordinateWrappers.hpp"
+#include "CurveTensor.hpp"
+#include "CurveTensor/Plane.hpp"
+#include "CurveTensor/Sphere.hpp"
+#include "CurveTensor/Torus.hpp"
+#include "EscapeCheck.hpp"
+#include "EscapeCheck/OutsideCircle.hpp"
+#include "RandDouble.hpp"
+#include "Step.hpp"
+#include "Stepper.hpp"
+#include "Walk.hpp"
