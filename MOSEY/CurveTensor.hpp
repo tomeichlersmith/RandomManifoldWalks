5707f11ce3855160edb9919c32fd458ff0a7d958
Tom Eichlersmith <tom.eichlersmith@gmail.com>
2018-01-07 18:01:06 -0600

    move back to function pointer version of christoffel symbols

--- MOSEY/CurveTensor.hpp
+++ MOSEY/CurveTensor.hpp
@@ -11,25 +11,76 @@ namespace MOSEY {
 	const long double TWO_PI = 2*PI;
 	
 	/**
+	 * Christoffel Symbol function type
+	 */
+	typedef double (ChristoffelSymbolPtr*)(double,double);
+	
+	/**
+	 * The zero funciton in Christoffel Symbol structure.
+	 */
+	double ZeroSymbol(double u, double v);
+	
+	/**
+	 * Sphere UUV Christoffel Symbol.
+	 * pi*cot( pi*v )
+	 */
+	double SphereUUVSymbol(double u, double v);
+	
+	/**
+	 * Sphere VUU Christoffel Symbol.
+	 * -2*pi*sin( 2*pi*v )
+	 */
+	double SphereVUUSymbol(double u, double v);
+	
+	/**
+	 * Torus UUV Christoffel Symbol.
+	 * (-2*pi*r*sin( 2*pi*v ))/(R+r*cos( 2*pi*v ))
+	 */
+	double TorusUUVSymbol(double u, double v);
+	
+	/**
+	 * Torus VUU Christoffel Symbol.
+	 * (2*pi/r)*(R+r*cos( 2*pi*v )*sin( 2*pi*v )
+	 */
+	double TorusVUUSymbol(double u, double v);
+	
+	/**
 	 * Curvature Tensor class. A short class that stores the Christoffel symbols for a two dimensional manifold.
 	 * Assumes Curvature Tensor is symmetric (i.e. uuv = uvu and vuv = vvu)
 	 */
 	class CurveTensor {
 		public:
 			/**
-			 * Constructor
+			 * Default Constructor. Sets all Christoffel Symbols to Zero Symbol (plane).
 			 */
 			CurveTensor();
 			
-			virtual double UUU(const double u, const double v) const;
-			virtual double UUV(const double u, const double v) const;
-			virtual double UVV(const double u, const double v) const;
-			virtual double VUU(const double u, const double v) const;
-			virtual double VUV(const double u, const double v) const;
-			virtual double VVV(const double u, const double v) const;
+			/**
+			 * Preferred Constructor. Sets Christoffel Symbols depending on input character.
+			 * S <=> sphere, T <=> torus, o.w. <=> plane
+			 */
+			CurveTensor( char signalchar );
+			
+			/**
+			 * General Constructor.
+			 */
+			CurveTensor( ChristoffelSymbolPtr uuu, ChristoffelSymbolPtr uuv, ChristoffelSymbolPtr uvv,
+									 ChristoffelSymbolPtr vuu, ChristoffelSymbolPtr vuv, ChristoffelSymbolPtr vvv );
+			
+			double UUU(double u, double v) const;
+			double UUV(double u, double v) const;
+			double UVV(double u, double v) const;
+			double VUU(double u, double v) const;
+			double VUV(double u, double v) const;
+			double VVV(double u, double v) const;
 		
 		private:
-			
+			ChristoffelSymbolPtr m_uuu;
+			ChristoffelSymbolPtr m_uuv;
+			ChristoffelSymbolPtr m_uvv;
+			ChristoffelSymbolPtr m_vuu;
+			ChristoffelSymbolPtr m_vuv;
+			ChristoffelSymbolPtr m_vvv;
 	};
 	
 }
--- src/CurveTensor.cpp
+++ src/CurveTensor.cpp
@@ -3,32 +3,82 @@
 
 namespace MOSEY {
 	
-	CurveTensor::CurveTensor() {
+	double ZeroSymbol(double u, double v) {
+		return 0;
+	}
+	
+	double SphereUUVSymbol(double u, double v) {
+		
+		double sinv = sin( PI*v );
+		double cosv = cos( PI*v );
+		
+		return ( PI*( cosv / sinv ) );
+	}
+	
+	double SphereVUUSymbol(double u, double v) {
+		return ( -1*TWO_PI*sin( TWO_PI*v ) );
+	}
+	
+	double TorusUUVSymbol(double u, double v) {
+		
+		double m_tube_rad = 1., m_hole_rad = 2.;
+		double num = -1*TWO_PI*m_tube_rad*sin( TWO_PI*v );
+		double den = m_hole_rad + m_tube_rad*cos( TWO_PI*v );
+		
+		return ( num / den );
+	}
+	
+	double TorusVUUSymbol(double u, double v) {
+		
+		double m_tube_rad = 1., m_hole_rad = 2.;
+		
+		return ( ( TWO_PI / m_tube_rad )*( m_hole_rad + m_tube_rad*cos( TWO_PI*v ) )*sin( TWO_PI*v ) );
+	}
+	
+	CurveTensor::CurveTensor() : m_uuu(&ZeroSymbol), m_uuv(&ZeroSymbol), m_uvv(&ZeroSymbol), 
+		m_vuu(&ZeroSymbol), m_vuv(&ZeroSymbol), m_vvv(&ZeroSymbol) {
 		/* Intentionally Empty */
 	}
 	
-	double CurveTensor::UUU(const double u, const double v) const {
-		return 0;
+	CurveTensor::CurveTensor( char signalchar ) : CurveTensor() {
+		
+		//Default constructor sets all symbols to zero symbol, so only change non-zero ones
+		switch ( signalchar ) {
+			case 'S': m_uuv = &SphereUUVSymbol; m_vuu = &SphereVUUSymbol;
+								break;
+			default : break; //Do Nothing
+		}
+		
 	}
 	
-	double CurveTensor::UUV(const double u, const double v) const {
-		return 0;
+	CurveTensor::CurveTensor( ChristoffelSymbolPtr uuu, ChristoffelSymbolPtr uuv, ChristoffelSymbolPtr uvv,
+									 					ChristoffelSymbolPtr vuu, ChristoffelSymbolPtr vuv, ChristoffelSymbolPtr vvv ) :
+		m_uuu(uuu), m_uuv(uuv), m_uvv(uvv), m_vuu(vuu), m_vuv(vuv), m_vvv(vvv) {
+		/* Intentionally Empty */
 	}
 	
-	double CurveTensor::UVV(const double u, const double v) const {
-		return 0;
+	double CurveTensor::UUU(double u, double v) const {
+		return m_uuu(u,v);
 	}
 	
-	double CurveTensor::VUU(const double u, const double v) const {
-		return 0;
+	double CurveTensor::UUV(double u, double v) const {
+		return m_uuv(u,v);
 	}
 	
-	double CurveTensor::VUV(const double u, const double v) const {
-		return 0;
+	double CurveTensor::UVV(double u, double v) const {
+		return m_uvv(u,v);
 	}
 	
-	double CurveTensor::VVV(const double u, const double v) const {
-		return 0;
+	double CurveTensor::VUU(double u, double v) const {
+		return m_vuu(u,v);
+	}
+	
+	double CurveTensor::VUV(double u, double v) const {
+		return m_vuv(u,v);
+	}
+	
+	double CurveTensor::VVV(double u, double v) const {
+		return m_vvv(u,v);
 	}
 	
 }
