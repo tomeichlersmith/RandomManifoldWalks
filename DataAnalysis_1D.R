bfc83b79c6ed10e654fe36b6d9a2c4d5884750f7
Tom Eichlersmith <tom.eichlersmith@gmail.com>
2018-02-12 12:33:19 -0600

    Researched format of data analysis

---
+++ DataAnalysis_1D.R
@@ -0,0 +1,11 @@
+# 1-dimensional data analysis
+# Useful in symmetrical cases where the problem can be reduced to one dimension
+# Import data in two column table: { point , distance of walk to escape }
+# Plots, "averages" data by interpolating
+
+library(ggplot2)
+
+#read.csv to import
+#ggplot to plot
+#model <- lm(data.y ~ poly(data.x,n))
+# n - polynomial degree
