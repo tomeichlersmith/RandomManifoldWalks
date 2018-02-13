6876e8c886125c379213b806e5e8ac2688ff1772
Tom Eichlersmith <tom.eichlersmith@gmail.com>
2018-02-13 10:16:56 -0600

    commented out personal fit (overloading on large data sets)

--- DataAnalysis_1D.R
+++ DataAnalysis_1D.R
@@ -15,17 +15,17 @@ gp <- ggplot( raw_walk , aes( x = R , y = WalkLen ) ) +
   geom_point( size = 0.5 , alpha = 0.05 )
 gp + geom_smooth()
 
-#Personal Model
-model <- loess( R ~ WalkLen , data = raw_walk )
-xrange <- range(  raw_walk$R )
-xseq <- seq( from = xrange[1] , to = xrange[2] , length = 100 )
-pred <- predict( model , newdata = data.frame( WalkLen = xseq ) , se = TRUE )
-y = pred$fit
-ci <- pred$se.fit * qt( 0.95 / 2 + 0.5 , pred$df )
-ymin = y - ci
-ymax = y + ci
-loess.DF <- data.frame( x = xseq , y , ymin , ymax , se = pred$se.fit )
+# #Personal Model
+# model <- loess( R ~ WalkLen , data = raw_walk )
+# xrange <- range(  raw_walk$R )
+# xseq <- seq( from = xrange[1] , to = xrange[2] , length = 100 )
+# pred <- predict( model , newdata = data.frame( WalkLen = xseq ) , se = TRUE )
+# y = pred$fit
+# ci <- pred$se.fit * qt( 0.95 / 2 + 0.5 , pred$df )
+# ymin = y - ci
+# ymax = y + ci
+# loess.DF <- data.frame( x = xseq , y , ymin , ymax , se = pred$se.fit )
+# 
+# gp +
+#   geom_smooth( aes_auto(loess.DF) , data = loess.DF , stat = "identity" )
 
-gp +
-  geom_smooth( aes_auto(loess.DF) , data = loess.DF , stat = "identity" )
-# n - polynomial degree
