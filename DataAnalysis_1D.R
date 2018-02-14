106c23b4da6e5e898e8b20fc435221f2b1e2f87c
Tom Eichlersmith <tom.eichlersmith@gmail.com>
2018-02-14 15:44:37 -0600

    updated data analysis in R, output to pdf file in data/ directory

--- DataAnalysis_1D.R
+++ DataAnalysis_1D.R
@@ -1,31 +1,31 @@
-?# 1-dimensional data analysis
+# 1-dimensional data analysis
 # Useful in symmetrical cases where the problem can be reduced to one dimension
 # Import data in two column table: { point , distance of walk to escape }
 # Plots, "averages" data by interpolating
 
 library(ggplot2)
 
+filename <- "Sphere_L03_S005_N100.csv"
+
 #Construct file path
-fp <- file.path("~/CodeProjects/MathDHP_201718/Data", "Sphere_0300000_0050000_50.csv")
+datadir <- "~/CodeProjects/MathDHP_201718/RandomManifoldWalks/data/"
+fp <- paste( datadir , filename , sep = "" )
 #read in csv file to a data frame
 raw_walk <- read.csv(fp)
 
-#Default ggplot fit
+#Default ggplot
 gp <- ggplot( raw_walk , aes( x = PolAng , y = WalkLen ) ) +
-  geom_point( size = 0.5 , alpha = 0.05 )
-gp + geom_smooth()
+  geom_point( size = 0.3 , alpha = 0.03 )
 
-# #Personal Model
-# model <- loess( R ~ WalkLen , data = raw_walk )
-# xrange <- range(  raw_walk$R )
-# xseq <- seq( from = xrange[1] , to = xrange[2] , length = 100 )
-# pred <- predict( model , newdata = data.frame( WalkLen = xseq ) , se = TRUE )
-# y = pred$fit
-# ci <- pred$se.fit * qt( 0.95 / 2 + 0.5 , pred$df )
-# ymin = y - ci
-# ymax = y + ci
-# loess.DF <- data.frame( x = xseq , y , ymin , ymax , se = pred$se.fit )
-# 
-# gp +
-#   geom_smooth( aes_auto(loess.DF) , data = loess.DF , stat = "identity" )
+#ggplot does the fit
+gp <- gp + geom_smooth()
 
+#Export to pdf file
+plotname <- gsub( "csv" , "pdf" , filename)
+plotpath <- paste( datadir , plotname , sep = "" )
+pdf( plotpath ) #Open File Connection
+gp
+dev.off() #Stop Printing
+
+#Data Clean Up
+rm( raw_walk , gp )
