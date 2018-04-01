131d4e4155e57c60b6211b05b93bad8e9b9768b4
Tom Eichlersmith <tom.eichlersmith@gmail.com>
2018-04-01 18:52:58 -0500

    two dimensional plot gen improvement

--- PlaneCircle/PlotGen.R
+++ PlaneCircle/PlotGen.R
@@ -12,7 +12,7 @@ if ( length(args) != 1 ) {
 }
 
 # Get filename
-filename <- args[1]
+filename <- "PlaneCircle_L05_N2000_S005" #args[1]
 
 library(ggplot2)
 
@@ -22,10 +22,13 @@ sum_fp <- paste( datadir , filename , "_summary.csv" , sep = "" )
 sum_walk <- read.csv( sum_fp )
 
 #Default ggplot
-gp <- ggplot() + geom_point( data = sum_walk , aes( x = U , y = V , color = MeanWalkLen ) )
-gp <- gp + xlab("Starting U Coordinate") +
-  ylab("Starting Y Coordinate")
-#  zlab("Length of Walk to Escape")
+gp <- ggplot(data = sum_walk , aes( x = U , y = V ) ) + xlab("Starting U Coordinate") +
+  ylab("Starting Y Coordinate") # + zlab("Length of Walk to Escape")
+gp + geom_raster(aes(fill = MeanWalkLen)) +
+  scale_fill_gradient(name = "Mean Length\nto Escape",
+                      trans = "log", #Change color scale to log
+                      breaks = c(0.1,1,10,100), #Set markers on log scale
+                      limits = c(0.01,150)) #Set limits (required to set markers)
 
 #Export to pdf file
 plotpath <- paste( datadir , filename , ".pdf" , sep = "" )
